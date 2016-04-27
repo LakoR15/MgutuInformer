fb786a33079f189f10f3c9e34907063998757d89
LakoR15 <alexvolkov1994@gmail.com>
2016-04-27 16:14:14 +0300

    убраны лишние поля из модели Users

--- app/src/main/java/ru/mgutupenza/mgutuinformer/model/server/Users.java
+++ app/src/main/java/ru/mgutupenza/mgutuinformer/model/server/Users.java
@@ -1,33 +1,14 @@
 package ru.mgutupenza.mgutuinformer.model.server;
 
-import java.util.UUID;
-
 public class Users {
 
     private Long usersId;
     private String secretKey;
     private String name;
     private Groups groups;
-    private String login;
-    private String password;
     private Boolean student;
 
-    public Users(String name, Groups groups, String login, String password, Boolean student) {
-        this.name = name;
-        this.groups = groups;
-        this.login = login;
-        this.password = password;
-        this.student = student;
-        this.secretKey = UUID.randomUUID().toString();
-    }
-
     public Users() {
-    }
-
-    public Users(String login, String password) {
-        this.login = login;
-        this.password = password;
-        this.secretKey = UUID.randomUUID().toString();
     }
 
     public Groups getGroups() {
@@ -60,22 +41,6 @@ public class Users {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public Boolean getStudent() {
